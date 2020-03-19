deb36bf6bb7fe4e4f33671c4f5c2056478f11014
Latishfaction <latishchambhare@gmail.com>
2020-03-19 18:16:30 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,29 @@
+import clearterm
+
+
+class a:
+    def feature1(self):
+        print('This is feature 1')
+
+    def feature2(self):
+        print('This is feature 2')
+
+
+class b:
+    def feature3(self):
+        print('This is feature 3')
+
+    def feature4(self):
+        print('This is feature 4')
+
+
+class c(a, b):
+    def feature5(self):
+        print('This is feature 5')
+
+    def feature6(self):
+        print('This is feature 6')
+
+
+cc = c()
+cc.feature6()
