650f900fffaf1f1dfd9b915dc424822e857dae2e
Latishfaction <latishchambhare@gmail.com>
2020-03-19 19:10:23 +0530

    constructor-inheritance

---
+++ constructor_inheritance.py
@@ -0,0 +1,41 @@
+'''Whenever a class runs it calls the __init__ method, if a class doesn't have __init__ method so it calls the __init__ method of super-class'''
+'''Method Resolution Order(MRO): It generally happens in 'Multiple-Inheritance', When we encounter same method name(including __init__) in the both of the super class, then it execute from left-to-right '''
+
+
+
+
+import clearterm
+class a:
+    def __init__(self):
+        print("Init A:")
+
+    def feature1(self):
+        print('This is feature 1-a')
+
+    def feature2(self):
+        print('This is feature 2')
+
+
+class b:
+    def __init__(self):
+        print("Init B:")
+
+    def feature1(self):
+        print('This is feature 1-b')
+
+    def feature4(self):
+        print('This is feature 4')
+
+
+class c(a, b):
+
+    def feature1(self):
+        print('This is feature 5')
+
+    def feature6(self):
+        print('This is feature 6')
+
+
+cb = b()
+cc = c()
+cc.feature1()
